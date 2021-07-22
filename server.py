9ce722a47510c3467b8a29c5f94f3d82592061ac
roughshodbasher <38942101+roughshodbasher@users.noreply.github.com>
2021-07-22 18:43:11 +1000

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
-s.bind(('0.0.0.0', 1024))        # Bind to the port
+s.bind(('0.0.0.0', 1025))        # Bind to the port
 
 s.listen(5)                 # Now wait for client connection.
 while True:
    c, addr = s.accept()     # Establish connection with client.
    print('Got connection from', addr)
-   data = c.recv(1024)
+   data = c.recv(1025)
    print(data)
    c.close()                # Close the connection
    if data.decode() == 'exit':
