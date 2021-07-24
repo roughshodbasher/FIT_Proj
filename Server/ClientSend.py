5107cf2635b3e923be451849364f43ec80c73ffb
roughshodbasher <38942101+roughshodbasher@users.noreply.github.com>
2021-07-24 14:35:02 +1000

    Updated Basic Client
    Added Return Message test in Server_Main.py

--- Server/ClientSend.py
+++ Server/ClientSend.py
@@ -1,12 +1,23 @@
 import socket               # Import socket module
 import time
 import json
-s = socket.socket()
-d = json.dumps(['test',{"test" : "exit"},123,False])
-#d = 'exit'
-ip = 'localhost'
-port = 1024
-s.connect((ip,port))
-s.sendall(bytes(d,encoding='utf-8'))
 
-s.close()
+def sendMessage(info=[]):
+    data = json.dumps(info)
+    s = socket.socket()
+    ip = 'localhost'
+    port = 1024
+    dataSize = 1024
+    s.connect((ip, port))
+    s.sendall(bytes(data, encoding='utf-8'))
+    d = s.recv(dataSize)
+    s.close()
+
+
+
+currentLocation = "32-38 Exhibition Walk, Clayton VIC 3800"
+destination = "20 Research Way, Clayton VIC 3800"
+vehicle = ("Toyota, Yarris, 2013")
+
+sendMessage([currentLocation,destination,vehicle])
+sendMessage('exit')
--- Server/Server_Main.py
+++ Server/Server_Main.py
@@ -1,6 +1,12 @@
 import socket  # Import socket module
 import json
 
+key = ''
+if not key:
+    with open("key.api") as f:
+        key = f.readline().rstrip()
+print(key)
+dataSize = 1024
 port = 1024
 s = socket.socket()  # Create a socket object
 s.bind(('0.0.0.0', port))  # Bind to the port
@@ -12,7 +18,8 @@ s.listen(5)  # Now wait for client connection.
 while True:
     c, addr = s.accept()  # Establish connection with client.
     print('Got connection from', addr)
-    data = c.recv(port)
+    data = c.recv(dataSize)
+    c.send(b"abc")
     print(data.decode())
     c.close()  # Close the connection
     if data.decode() == 'exit':
